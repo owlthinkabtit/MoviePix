1d714cc57f446c21b471e71ba82cbdfe323ee9a5
owlthinkabtit <haggins.jameka@gmail.com>
2024-08-13 19:59:53 -0400

    everything is in working order, will start working on responsiveness tomorrow

--- lethimcook.js
+++ lethimcook.js
@@ -4,12 +4,12 @@ const API_KEY = 'api_key=e87a0dde459d4dbbc77f2e6343e3cfc8';
 const BASE_URL = 'https://api.themoviedb.org/3';
 const API_URL = BASE_URL + '/discover/movie?sort_by=popularity.desc&' + API_KEY;
 const IMG_URL = 'https://image.tmdb.org/t/p/w500';
-const searchURL = BASE_URL + '/search/movie?'+API_KEY;
+const searchURL = BASE_URL + '/search/movie?' + API_KEY;
 
 
-const main = document.getElementById('movies');
-const form = document.getElementsByClassName('search-bar');
-const search = document.getElementsByClassName('search-bar');
+const main = document.getElementById('app');
+const form = document.getElementById('form');
+const search = document.getElementsByClassName('search')[0];
 
 getMovies(API_URL);
 
@@ -26,11 +26,11 @@ function showMovies(data) {
   movies.innerHTML = "";
 
   data.forEach(movie => {
-    const {title, poster_path, vote_average, overview} = movie;
+    const { title, poster_path, vote_average, overview } = movie;
     const movieEl = document.createElement('div');
     movieEl.classList.add('movie');
     movieEl.innerHTML = `
-      <img class="movie-img" src="${IMG_URL+poster_path}" alt="${title}" />
+      <img class="movie-img" src="${IMG_URL + poster_path}" alt="${title}" />
         <div class="movie-info">
           <h3>${title}</h3>
             <span class=${getColor(vote_average)}>${vote_average.toFixed(1)}</span>
@@ -40,27 +40,41 @@ function showMovies(data) {
             ${overview};
         </div>
       `
-      movies.appendChild(movieEl);
+    movies.appendChild(movieEl);
   })
 
 }
 
 function getColor(vote) {
-  if(vote >= 8){
+  if (vote >= 8) {
     return 'green'
-  }else if(vote >= 5){
+  } else if (vote >= 5) {
     return "orange"
-  }else{
+  } else {
     return 'red'
   }
 }
 
-searchMovie.addEventListener('onclick', (e) => {
+/*form.addEventListener("submit", (e) => {
    e.preventDefault();
 
    const searchTerm = search.value;
 
-   if(searchTerm) {
-    getMovies(searchURL+'&query='+searchTerm)
-   };
+   if(searchTerm){
+      getMovies(searchURL+'&query='+searchTerm)
+   }
+}); */
+
+document.addEventListener('DOMContentLoaded', () => {
+  const form = document.getElementById('form');
+  if (form) {
+    form.addEventListener("submit", (e) => {
+      e.preventDefault();
+      const searchTerm = search.value;
+      if (searchTerm) {
+        getMovies(searchURL + '&query=' + searchTerm)
+      }
+    });
+  }
 });
+
