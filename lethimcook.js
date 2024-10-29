4b4201e1ea4687ec964cbbec37af8f12bfc213bb
owlthinkabtit <haggins.jameka@gmail.com>
2024-10-29 14:41:51 -0400

    because I changed my API key for a different project, it was no longer working for this one. So I decided to fix that on this project as well.

--- lethimcook.js
+++ lethimcook.js
@@ -1,6 +1,6 @@
 //using the api on themoviedatabase.org 
 
-const API_KEY = 'api_key=e87a0dde459d4dbbc77f2e6343e3cfc8';
+const API_KEY = 'api_key=8286b6390cb7e70d35486355cf608d8d';
 const BASE_URL = 'https://api.themoviedb.org/3';
 const API_URL = BASE_URL + '/discover/movie?sort_by=popularity.desc&' + API_KEY;
 const IMG_URL = 'https://image.tmdb.org/t/p/w500';
